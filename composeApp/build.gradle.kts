c4919cd38e362103f35feeee80f3eb1c03c0fd89
Pedro Malta <pedro.costa@globalsys.com.br>
2024-11-19 12:15:40 -0300

    Add ReactiveX impl

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -90,6 +90,10 @@ kotlin {
             implementation(libs.ktor.client.content.negotiation)
             implementation(libs.ktor.client.encoding)
 
+            implementation(libs.reaktive)
+            implementation (libs.reaktive.annotations)
+            implementation (libs.reaktive.coroutines.interop)
+
         }
     }
 }
