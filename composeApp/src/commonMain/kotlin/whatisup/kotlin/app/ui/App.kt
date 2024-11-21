37fc99be1af8fad4e14ea6056b6d24f44c1f6e09
Pedro Malta <pedro.costa@globalsys.com.br>
2024-11-21 10:22:14 -0300

    Fix app after lib changes. All working point

--- composeApp/src/commonMain/kotlin/whatisup/kotlin/app/ui/App.kt
+++ composeApp/src/commonMain/kotlin/whatisup/kotlin/app/ui/App.kt
@@ -4,16 +4,19 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.navigation.compose.rememberNavController
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinApplication
+import org.koin.compose.koinInject
 import whatisup.kotlin.app.di.DataModules
+import whatisup.kotlin.app.domain.DataSource
 import whatisup.kotlin.app.domain.Greeting
 
 import whatisupkotlin.composeapp.generated.resources.Res
@@ -29,17 +32,17 @@ fun App() {
             })
         })
     {
-        val navigator = rememberListDetailPaneScaffoldNavigator<MyItem>()
-
-        BackHandler(navigator.canNavigateBack()) {
-            navigator.navigateBack()
-        }
-
+        val dataSource = koinInject<DataSource>()
         MaterialTheme {
+
+
+            val navController = rememberNavController()
+
             var showContent by remember { mutableStateOf(false) }
             Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
                 Button(onClick = { showContent = !showContent }) {
                     Text("Click me!")
+                    dataSource.fetchRepoList(1)
                 }
                 AnimatedVisibility(showContent) {
                     val greeting = remember { Greeting().greet() }
