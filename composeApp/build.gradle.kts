612bc6bfafe5c55964549235ac947dd0d328d754
Pedro Malta <pedro.costa@globalsys.com.br>
2024-11-21 10:19:40 -0300

    Fix and organize libs

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -2,10 +2,10 @@ import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.composeMultiplatform)
-    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.serialization)
     alias(libs.plugins.ksp)
     alias(libs.plugins.room)
@@ -66,33 +66,22 @@ kotlin {
         commonMain.dependencies {
             implementation(compose.runtime)
             implementation(compose.foundation)
-            implementation(compose.material)
             implementation(compose.ui)
             implementation(compose.components.resources)
+            implementation(compose.material3)
+            implementation(compose.materialIconsExtended)
             implementation(compose.components.uiToolingPreview)
-            implementation(libs.androidx.lifecycle.viewmodel)
-            implementation(libs.androidx.lifecycle.runtime.compose)
-
-            implementation(libs.androidx.room.runtime)
-            implementation(libs.sqlite.bundled)
 
             implementation(libs.kotlinx.coroutines.core)
             implementation(libs.kotlinx.datetime)
             implementation(libs.kotlinx.serialization.json)
+            implementation(libs.androidx.navigation.compose)
 
-            implementation(libs.koin.core)
-            implementation(libs.koin.compose)
-            implementation(libs.koin.compose.viewmodel)
-
-            implementation(libs.ktor.client.core)
-            implementation(libs.ktor.client.logging)
-            implementation(libs.ktor.serialization.kotlinx.json)
-            implementation(libs.ktor.client.content.negotiation)
-            implementation(libs.ktor.client.encoding)
-
-            implementation(libs.reaktive)
-            implementation (libs.reaktive.annotations)
-            implementation (libs.reaktive.coroutines.interop)
+            implementation(libs.bundles.compose.common)
+            implementation(libs.bundles.koin.common)
+            implementation(libs.bundles.reaktive.common)
+            implementation(libs.bundles.ktor.common)
+            implementation(libs.bundles.room.common)
 
         }
 
@@ -103,25 +92,17 @@ kotlin {
             implementation(libs.reaktive.testing)
         }
 
-        androidUnitTest.dependencies {
-            implementation(libs.kotlin.test)
-            implementation(libs.kotlinx.coroutines.test)
-            implementation(libs.junit)
-            implementation(libs.reaktive.testing)
-        }
-
-
     }
 }
 
 android {
     namespace = "whatisup.kotlin.app"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    compileSdk = libs.versions.android.compilesdk.get().toInt()
 
     defaultConfig {
         applicationId = "whatisup.kotlin.app"
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        minSdk = libs.versions.android.minsdk.get().toInt()
+        targetSdk = libs.versions.android.targetsdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
     }
